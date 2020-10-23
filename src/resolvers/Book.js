f42ca54f879c3c579b6217117480eb638efc3417
Javier Rodriguez <jbeats.rtm@gmail.com>
2020-10-23 15:39:09 -0600

    Books Query Add

--- src/resolvers/Book.js
+++ src/resolvers/Book.js
@@ -1,6 +1,9 @@
 const Book = {
     write_by: (parent, args, {db}, info) => {
         return db.authors.find(author => author.id === parent.write_by)
+    },
+    register_by: (parent, args, {db}, info) => {
+        return db.users.find(user => user.id === parent.register_by)
     }
 }
 
