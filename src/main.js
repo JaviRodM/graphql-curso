40f052d5044b967051b46b01ea681be0ff351d0c
Javier Rodriguez <jbeats.rtm@gmail.com>
2020-10-28 13:33:37 -0600

    Creado primer Subscription

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import Query from './resolvers/Query'
 import db from './db'
 import Author from './resolvers/Author'
 import Book from './resolvers/Book'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
+
+
+const pubsub = new PubSub()
 
 const resolvers = {
     Query,
     Author,
     Book,
     Mutation,
+    Subscription
 }
 
 const context = {
-  db
+  db,
+  pubsub,
 }
 
 const server = new GraphQLServer({
